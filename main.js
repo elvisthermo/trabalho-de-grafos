337febdfaa2b43d19596a69b3a61aa71e60b5179
elvis <elvisthermo@hotmail.com>
2018-06-27 16:56:49 -0300

    menu ok so falta adicionar as funções
    menu funcionando mas ainda falta as colocar as funções

--- main.js
+++ main.js
@@ -1,65 +1,10 @@
 let menu = require('node-menu');
+const colors = require('colors');
 let Graph = require("./graphs.js")
+let menu2 = menu.menu2;
 
 let g = Graph.g;
 let grafo1 = new Graph(5);
-//console.log(grafo1);
-
-console.log("-montar grafo:");
-console.log("1 -grafo direcionado");
-console.log("2 -grafo não direcionado");
-console.log("0 - exit");
-
-console.log("1 - Adicionar Vertices");
-console.log("2 - Adicionar Arestas");
-console.log("2 - Remover Arestas");
-console.log("3 - Chekar aresta");
-console.log("4 - Grau vertice");
-console.log("5 - Remover Vertices");
-console.log("0 - exit");
-
-console.log("1-  Busca em largura");
-console.log("2 - Busca em profundidade");
-console.log("3 - Ordenação topologica");
-console.log("4 - Djsktra");
-console.log("5 - Bellman-ford");
-console.log("6 - Floyd-warshall");
-console.log("7 - Componentes fortemete conexos");
-console.log("7 - é Euleriano");
-console.log("9 - Prim");
-console.log("8 - Kruskall");
-console.log("0 - exit");
-/*
-console.log("1 -buscas");
-console.log("2 -caminho minimo");
-console.log("3 -Arvore geradora Minima");
-*/
-
-/*
-let resp;
-
-switch (resp) {
-    case 0:
-        break;
-    case 1:
-
-        break;
-    case 2:
-
-        break;
-    case 3:
-
-        break;
-    case 4:
-
-        break;
-    case 5:
-
-        break;
-    case  6:
-
-}
-*/
 
 //console.log(graph);
 //g = new graph(1);
@@ -77,48 +22,195 @@ TestObject.prototype.printFieldB = function(arg) {
     console.log(this.fieldB + arg);
 }
 
-var testObject = new TestObject();
-menu.addDelimiter('-', 40, 'Trabalho de grafos')
-    .addItem(
-        'Montar grafo',
-        function() {
-            console.log('1 - Adicionar vertices ');
-            console.log('2 - Adicionar arrestas');
-            console.log('3 - Remover Vertices');
-            console.log('4 - Remover Remover Aresta');
-            console.log("0 - exit");
-        })
-    .addItem(
-        "buscas em grafos",function(){
-            console.log("1-  Busca em largura");
-            console.log("2 - Busca em profundidade");
-            console.log("3 - Ordenação topologica");
-            console.log("4 - Componentes fortemete conexos");
-            console.log("5 - Chekar vertices");
-            console.log("6 - Chekar arestas");
-            console.log("7 - Grau verice");
-            console.log("8 - Vizinhança do vertice");
+menuIniciar();
 
-            console.log("0 - exit");
-        })
-    .addItem(
-        'custo Minimo',function(){
-            console.log("1 - Dijkstra");
-            console.log("2 - Bellman-ford");
-            console.log("3 - Floyd-warshall");
-            console.log("0 - exit");
 
-        })
-    .addItem(
-        'Arvore Geradora Minima',function(){
-            console.log("1 - Prim");
-            console.log("2 - Kruskall");
-            console.log("0 - exit");
+function menuIniciar() {
+        menu.resetMenu();
+        var testObject = new TestObject();
+        menu.addDelimiter('-', 40, colors.black.bgCyan('Trabalho de grafos( ͡° ͜ʖ ͡°) '))
+            .addItem(
+                'Grafo direcionado',
 
+                function () {
+                    menuPrincipal();
+
+                })
+            .addItem(
+                'Grafo não direcionado', function () {
+                    menuPrincipal();
+
+                })
+
+            .disableDefaultHeader()
+            .customPrompt(function () {
+                process.stdout.write("\nEscolha sua alternativa:\n");
+            })
+            .disableDefaultPrompt()
+            .start();
+
+}
+
+function menuPrincipal() {
+    menu.resetMenu();
+    var testObject = new TestObject();
+    menu.addDelimiter('-', 40, colors.black.bgCyan('Trabalho de grafos ( ͡° ͜ʖ ͡°) '))
+        .addItem(
+            'Montar grafo',
+
+            function () {
+                submenu1();
+
+            })
+        .addItem(
+            "buscas em grafos", function () {
+                submenu2();
+
+            })
+        .addItem(
+            'custo Minimo', function () {
+                submenu3();
+
+            })
+        .addItem(
+            'Arvore Geradora Minima', function () {
+                submenu4()
+
+            })
+        .addItem(
+            'Voltar', function () {
+                menuIniciar();
+
+            })
+        .disableDefaultHeader()
+        .customPrompt(function () {
+            process.stdout.write("\nEscolha sua alternativa:\n");
         })
-     .disableDefaultHeader()
-    .customPrompt(function() {
-         process.stdout.write("\nEnter your selection:\n");
-     })
-    .disableDefaultPrompt()
-    .start();
+        .disableDefaultPrompt()
+        .start();
+}
+
+function submenu1() {
+    menu.resetMenu();
+     menu.addDelimiter('-', 40, colors.black.bgBlue('Estrutura do Grafo'))
+        .addItem(
+            'Adicionar vertices',
+            function () {
+
+            })
+        .addItem(
+            'Adicionar arestas',
+            function () {
+
+            })
+        .addItem(
+            'remover Vertices',
+            function () {
+
+        }).addItem(
+        'Remover Arestas',
+        function () {
+
+        }).addItem(
+             'Mostrar Grafo',
+             function () {
+             })
+         .addItem(
+             'voltar',
+             function () {
+                 menuPrincipal()
+             })
+         .start();
+}
+
+function submenu2() {
+    menu.resetMenu();
+    menu.addDelimiter('-', 40, colors.black.bgGreen('Busca em Grafos'))
+        .addItem(
+            'Busca em profundidade',
+            function () {
+
+            })
+        .addItem(
+            'Busca em largura',
+            function () {
+
+            })
+        .addItem(
+            'Ordenação topologica',
+            function () {
+
+            }).addItem(
+        'Componentes fortemete conexos',
+        function () {
+        })
+        .addItem(
+        'Chekar vertices',
+        function () {
+    })
+    .addItem(
+        'Chekar arestas',
+        function () {
+    })
+    .addItem(
+            'Grau do vertice',
+            function () {
+            })
+    .addItem(
+          'Visinhanca do vertice',
+          function () {
+          })
+    .addItem(
+            'voltar',
+            function () {
+                menuPrincipal()
+            })
+        .start();
+}
+
+function submenu3() {
+    menu.resetMenu();
+    menu.addDelimiter('-', 40, colors.black.bgRed('Custo Minimo'))
+        .addItem(
+            'Dijkstra',
+            function () {
+
+            })
+        .addItem(
+            'Bellman-ford',
+            function () {
+
+            })
+        .addItem(
+            'Floyd-warshall',
+            function () {
+
+            })
+        .addItem(
+            'voltar',
+            function () {
+                menuPrincipal()
+            })
+        .start();
+}
+
+function submenu4() {
+    menu.resetMenu();
+    menu.addDelimiter('-', 40, colors.black.bgYellow('Arvore Geradora Minima'))
+        .addItem(
+            'Prim',
+            function () {
+
+            })
+        .addItem(
+            'Kruslkall',
+            function () {
+
+            })
+        .addItem(
+            'voltar',
+            function () {
+                menuPrincipal()
+            })
+        .start();
+}
